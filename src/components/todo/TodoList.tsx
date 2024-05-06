66df79d02d36e9f4db89a59fcad9cc5a81718525
Mansi <jayswalmansi20@gmail.com>
2024-05-06 14:33:13 +0530

    [FEAT]: added localstorage to persist the data on reload

--- src/components/todo/TodoList.tsx
+++ src/components/todo/TodoList.tsx
@@ -1,4 +1,4 @@
-import {ChangeEvent, FormEvent, useState} from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 
 interface Todo {
   id: number;
@@ -7,15 +7,22 @@ interface Todo {
 }
 
 function TodoList() {
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todos, setTodos] = useState<Todo[]>(() => {
+    const storedTodos = localStorage.getItem("todos");
+    return storedTodos ? JSON.parse(storedTodos) : [];
+  });
   const [todoText, setTodoText] = useState<string>("");
-  const [searchQuery,setSearchQuery] = useState<string>("");
+  const [searchQuery, setSearchQuery] = useState<string>("");
 
-  const handleAddTodo = (e:FormEvent<HTMLFormElement>) => {
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const handleAddTodo = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (todoText.trim() !== "") {
       const newTodo: Todo = {
-        id: todos.length     + 1,
+        id: todos.length + 1,
         text: todoText,
         completed: false,
       };
@@ -42,68 +49,91 @@ function TodoList() {
 
   const searchTodo = (todos: Todo[]) => {
     return todos.filter((todo) => todo.text.includes(searchQuery));
-  }
-  
+  };
+
   return (
     <div className="text-center mt-8 w-[min(500px,90%)] mx-auto flex flex-col">
-      <h1 className="font-semibold text-purple-900 text-3xl mt-4 mb-2">TODO list with React & TS</h1>
+      <h1 className="font-semibold text-purple-900 text-3xl mt-4 mb-2">
+        TODO list with React & TS
+      </h1>
       <div className="flex justify-center align-center">
-        
-        <form onSubmit={handleAddTodo} className="flex gap-2 w-[95%] mx-auto justify-center items-center">
-        <input
-          type="text"
-          placeholder="add todo here..."
-          className="border-2 p-4 border-gray-700"
-          value={todoText}
-          onChange={(e:ChangeEvent<HTMLInputElement>) => setTodoText(e.target.value)}
-        />
-        <button type="submit"
-          className="border-2 rounded-lg bg-slate-400 p-2 text-black px-8 py-4 hover:bg-gray-300">Add</button>
+        <form
+          onSubmit={handleAddTodo}
+          className="flex gap-2 w-[95%] mx-auto justify-center items-center"
+        >
+          <input
+            type="text"
+            placeholder="add todo here..."
+            className="border-2 p-4 border-gray-700"
+            value={todoText}
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setTodoText(e.target.value)
+            }
+          />
+          <button
+            type="submit"
+            className="border-2 rounded-lg bg-slate-400 p-2 text-black px-8 py-4 hover:bg-gray-300"
+          >
+            Add
+          </button>
         </form>
       </div>
       <div className="text-center w-full mt-4">
-      <input
+        <input
           type="text"
           placeholder="Search..."
           className="border-2 mt-2 mr-2 p-4 border-gray-700"
           value={searchQuery}
-          onChange={(e:ChangeEvent<HTMLInputElement>) => handleSearch(e.target.value)}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            handleSearch(e.target.value)
+          }
         />
-    {
-      todos.length===0 ? <div className="mt-4 "><h1 className="text-3xl font-semibold
-       text-purple-400">No tasks to do!</h1></div> 
-      :
-
-    
-      <ul className="mt-4 space-y-2">
-        {searchTodo(todos).map((todo) => (
-          <li
-            key={todo.id}
-            className={`flex items-center justify-between h-16 rounded-lg px-4 py-2 w-[80%] mx-auto
-           ${todo.completed? "bg-purple-300" : "bg-purple-200" }`}>
-            <span className={`flex-grow ${todo.completed ? "line-through" : ""}`}>{todo.text}</span>
-            <div className="flex space-x-2">
-              <button
-                className={`px-3 py-1 rounded-md ${
-                  todo.completed
-                    ? "bg-gray-400 text-gray-700"
-                    : "bg-blue-500 text-white"
-                }`}
-                onClick={() => handleCompleteTodo(todo.id)}
+        {todos.length === 0 ? (
+          <div className="mt-4 ">
+            <h1
+              className="text-3xl font-semibold
+       text-purple-400"
+            >
+              No tasks to do!
+            </h1>
+          </div>
+        ) : (
+          <ul className="mt-4 space-y-2">
+            {searchTodo(todos).map((todo) => (
+              <li
+                key={todo.id}
+                className={`flex items-center justify-between h-16 rounded-lg px-4 py-2 w-[80%] mx-auto
+           ${todo.completed ? "bg-purple-300" : "bg-purple-200"}`}
               >
-                {todo.completed ? "Undo" : "Complete"}
-              </button>
-              <button
-                className="px-3 py-1 rounded-md bg-red-500 text-white"
-                onClick={() => handleDeleteTodo(todo.id)}
-              >
-              Delete
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
-}
+                <span
+                  className={`flex-grow ${
+                    todo.completed ? "line-through" : ""
+                  }`}
+                >
+                  {todo.text}
+                </span>
+                <div className="flex space-x-2">
+                  <button
+                    className={`px-3 py-1 rounded-md ${
+                      todo.completed
+                        ? "bg-gray-400 text-gray-700"
+                        : "bg-blue-500 text-white"
+                    }`}
+                    onClick={() => handleCompleteTodo(todo.id)}
+                  >
+                    {todo.completed ? "Undo" : "Complete"}
+                  </button>
+                  <button
+                    className="px-3 py-1 rounded-md bg-red-500 text-white"
+                    onClick={() => handleDeleteTodo(todo.id)}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
